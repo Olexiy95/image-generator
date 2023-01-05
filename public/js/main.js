9c5ef8b139959c9e9cb71fd441be71bfb84636ad
Olexiy <alexpiroshenko@hotmail.com>
2023-01-05 22:21:38 +1100

    initial commit

---
+++ public/js/main.js
@@ -0,0 +1,65 @@
+function onsubmit(e){
+    e.preventDefault();
+
+    document.querySelector('.msg').textContent = "";
+    document.querySelector('#image').src = "";
+
+    const prompt = document.querySelector('#prompt').value
+    const size = document.querySelector('#size').value
+
+    if (prompt === '') {
+        alert("please add some text")
+        return
+    }
+
+    console.log("Success", prompt, size)
+
+    generateImageRequst ( prompt, size);
+}
+
+async function generateImageRequst(prompt, size){
+    try{
+        showSpinner();
+
+        const response = await fetch('/openai/generateimage', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                prompt,
+                size
+            })
+        })
+
+        if (!response.ok) {
+            removepinner();
+            throw new Error('That image could nto be generated');
+        }
+
+        const data = await response.json();
+        console.log(data)
+
+        const imageUrl = data.data;
+
+        document.querySelector('#image').src = imageUrl;
+        removepinner();
+
+
+    } catch (error){
+        document.querySelector('.msg').textContent = error;
+
+    }
+
+}
+
+function showSpinner() {
+    document.querySelector('.spinner').classList.add('show');
+}
+
+function removepinner() {
+    document.querySelector('.spinner').classList.remove('show');
+}
+
+document.querySelector('#image-form').addEventListener('submit', onsubmit);
+
